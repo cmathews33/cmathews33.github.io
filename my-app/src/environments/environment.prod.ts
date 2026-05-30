f6180641e6a3a35527bcae7469395dfce155707a
cmathews33 <117398946+cmathews33@users.noreply.github.com>
2026-05-30 13:05:46 -0400

    Remove trailing slash from apiBaseUrl

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 // Production: set this to the API Gateway URL from the SAM stack `ApiUrl` output.
 export const environment = {
-  apiBaseUrl: 'https://za34t43kk6.execute-api.us-east-1.amazonaws.com/',
+  apiBaseUrl: 'https://za34t43kk6.execute-api.us-east-1.amazonaws.com',
 };
